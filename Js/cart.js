181545fcbd5a5baacef3ec65936d05d7255dfd9c
Evy <evetricia.k@hotmail.fr>
2021-05-07 12:47:07 +0200

    page panier en cours, récupération des articles pour affichage sur page panier ok

--- Js/cart.js
+++ Js/cart.js
@@ -5,33 +5,87 @@ const apiUrlRescue = "https://jwdp5.herokuapp.com/api/cameras/"; //url de secour
 let cameras = "cameras"; //Choix du Produit à vendre.
 
 /*******************************Page-3**Le Panier*******************************/
+//récupération des articles dans le localStorage
 
-const cartItems = document.getElementsByClassName("product");
-cartItems.createElement("div");
-cartItems.createElement("i");
+//boucle me permettant de récuperer les articles présent dans le local storage ainsi que l'ensemble des informations lié à chaque article grace a la clé et de les afficher dans la console
+// function cartItems() {
+
+// let key;
+for (let i = 0; i < localStorage.length; i++) {
+  console.log(i);
+  let key = localStorage.key(i);
+  console.log(key, localStorage.getItem(key));
+  let itemsInCart = JSON.parse(localStorage.getItem(key));
+  console.log('jkhkj :', itemsInCart);
+
+  //création de la div recevant les articles
+  let cartItemList = document.createElement("div");
+  cartItemList.classList.add("products__list", "col-4");
+
+  let deleteIcon = document.createElement("i");
+  deleteIcon.setAttribute("class", "fas fa-times-circle");
+
+  let itemImg = document.createElement("img");
+  itemImg.setAttribute("src", itemsInCart.url);
+  itemImg.classList.add("itemImg");
+
+  let itemName = document.createElement("span");
+  itemName.innerHTML = itemsInCart.nom;
+
+  let itemPrice = document.createElement("div");
+  itemPrice.classList.add("price", "col-2");
+
+  let price = document.createElement("span");
+  price.innerHTML = itemsInCart.prix / 100 + ',00€';
+
+  let itemQuantity = document.createElement("div");
+  itemQuantity.classList.add("quantity", "col-3");
+
+  let quantityCursorLeft = document.createElement("i");
+  quantityCursorLeft.setAttribute("class", "fas fa-chevron-circle-left");
+
+  let quantityInput = document.createElement("input");
+  quantityInput.id = "quantityInput";
+  quantityInput.setAttribute("type", "number");
+  quantityInput.setAttribute("name", "quantity");
+  quantityInput.setAttribute("value", itemsInCart.quantite);
+  quantityInput.setAttribute("min", "1");
+  quantityInput.setAttribute("max", "5");
+
+  let quantityCursorRight = document.createElement("i");
+  quantityCursorRight.setAttribute("class", "fas fa-chevron-circle-right");
+
+  let total = document.createElement("div");
+  itemPrice.classList.add("total", "col-2");
+
+  let totalPrice = document.createElement("span");
+  totalPrice.innerHTML = (itemsInCart.prix / 100) * itemsInCart.quantite + ',00€';
+
+  document.getElementById('cartItems').append(cartItemList, itemPrice, itemQuantity, total);
+  cartItemList.append(deleteIcon, itemImg, itemName)
+  //  );
+  itemPrice.append(price);
+  itemQuantity.append(quantityCursorLeft, quantityInput, quantityCursorRight);
+  total.append(totalPrice);
 
 
-/* < div class="product" >
-  <i class="fas fa-times-circle"></i>
-  <img src= ${camera.imageUrl}>
-    <span>${camera.name}</span>
-</div>
-  <div class="price">${camera.price},00€</div>
-  <div class="quantity">
-    <i class="fas fa-chevron-circle-left"></i>
-    <span>${camera.inCart}</span>
-    <i class="fas fa-chevron-circle-right"></i>
-  </div>
-  <div class total>
-    ${camera.inCart * camera.price},00€
-</div>`
+}
+
+// for (let i = 0; i < localStorage.length; i++) {
+//   console.log(i);
+//   let key = localStorage.key(i);
+//   console.log(key, localStorage.getItem(key));
+//   let totalCost = JSON.parse(localStorage.getItem(key));
+//   console.log('jkhkj :', totalCost);
+//   totalCost(totalCost);
+// }
+
+
+
+////////////form/////////////////
+let submit = document.getElementById("inscription").addEventListener("submi", function (e) {
+  e.preventDefault(); // annule le comportement par défaut du form
+  alert("Le formulaire à bien été envoyé !.");
 });
-productContainer.innerHTML +=
-` < div class= basketTotalContainer >
-    <h4 class="basketTotalTitle">
-      Basket Total
-  <h4>
-        <h4 class="basketTotal">
-          ${cartCost},00€
-  </h4>
-  </div>` */
+
+
--- Js/product.js
+++ Js/product.js
@@ -56,7 +56,6 @@ function getLocalStorageEltById(id, option) {
 
   //boucle permettant de parcourir le localStorage
   for (i = 0; i < localStorage.length; i++) {
-
     //de récupérer la clé des éléments présents.
     let element = localStorage.getItem(localStorage.key(i));
     //la clé (element) qui est un string, est enregistrée dans la variable elt sous forme d'objet grâce à JSON.parse.
@@ -71,6 +70,7 @@ function getLocalStorageEltById(id, option) {
   }
   // sinon l'élément non trouvé = null
   return null;
+
 }
 
 
@@ -168,7 +168,7 @@ function displayCameraById(camera) {
   btn.classList.add("list__btn", "btn", "add-cart", "mb-1", "col-8", "col-sm-5", "col-md-4", "col-md-5", "center", "text-light");
   btn.innerText = "Ajouter au panier";
 
-  //Ajout des articles aux paniers et définition des fonctionnalitées enclanchée au clic
+  //Ajout des articles aux paniers et définition des fonctionnalitées enclanchées au clic
   let addToCart = btn;
   addToCart.addEventListener("click", () => {
 
@@ -192,15 +192,15 @@ function displayCameraById(camera) {
     let compteur = document.getElementById("howManyInBag");
 
     console.log(localStorageElt); //affiche si l'article selectionné est déja présent ou null
-    console.log(compteur.innerHTML) //affiche le nombre d'article au compteur 
+    console.log(compteur.innerHTML); //affiche le nombre d'article au compteur 
 
     //si null => l'objet correspondant aux données envoyées lors du clic est créé dans le localStorage sous forme de chaine
     if (localStorageElt === null) {
       localStorage.setItem(localStorage.length, JSON.stringify(item));
 
-      //permet d"afficher le nombre d"aricle ajouté au panier au niveau de la barre de navigation grace au nombre d'éléments comptés dans le localStorage.
+      //permet d"afficher le nombre d'article ajouté au panier au niveau de la barre de navigation grace au nombre d'éléments comptés dans le localStorage.
       compteur.innerHTML = localStorage.length;
-      alert("L\"article à bien été ajouté au panier");
+      alert("L'article à bien été ajouté au panier");
 
     } else {
 
@@ -227,14 +227,16 @@ function displayCameraById(camera) {
       compteur.innerHTML = localStorage.length;
       alert("L'article à bien été ajouté au panier");
     }
+
   });
 
+
   //ajout de chaque élément ci-dessus au html
+  document.getElementById("cameraId").append(newDiv);
   newDiv.append(newUl);
   newUl.append(newLi);
   newLi.append(title, image, paragraph, price, containerChoice, btn);
   containerChoice.append(quantity, lensesTitle);
   quantity.append(quantityInput);
   lensesTitle.append(selectOption);
-  document.getElementById("cameraId").append(newDiv);
 };
